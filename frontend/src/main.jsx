681d0433bfca0bf7d956dea7fb3f4edbf5b87e90
Abdullokh8381 <abdullokhasrarov@gmail.com>
2026-05-04 01:55:07 +0500

    Integrated Google Client ID and activated Google Login

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,8 +4,8 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from "./App";
 import "./index.css";
 
-// Google Client ID ni keyinchalik .env orqali boshqaramiz
-const GOOGLE_CLIENT_ID = "CLIENT_ID_SHU_YERGA_QOYILADI";
+// Google Client ID
+const GOOGLE_CLIENT_ID = "690088711703-e647j0mbn8lv73ue6dr9cbur3n7ticlm.apps.googleusercontent.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
